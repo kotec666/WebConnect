fc7e85e2687a4278a98da7689d8f117e1be8e1fa
kotec666 <alexkotov639@gmail.com>
2024-04-19 03:02:33 +0400

    left panel

--- web/src/app/auth/page.tsx
+++ web/src/app/auth/page.tsx
@@ -1,16 +1,13 @@
 import React from "react"
-import Layout from "@/components/Layout"
 import LoginFormsWrapper from "@/app/auth/components/LoginFormsWrapper"
 import OAuthButtons from "@/app/auth/components/OAuthButtons"
 
 const Page = () => {
 	return (
-		<Layout>
-			<div className='flex flex-col gap-y-[30px] justify-center items-center h-[calc(100vh-70px)]'>
-				<LoginFormsWrapper />
-				<OAuthButtons />
-			</div>
-		</Layout>
+		<div className='flex flex-col gap-y-[30px] justify-center items-center h-[calc(100vh-70px)]'>
+			<LoginFormsWrapper />
+			<OAuthButtons />
+		</div>
 	)
 }
 
