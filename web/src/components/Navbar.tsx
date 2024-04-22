65a9b2a464e87b68f6265627e0770c30d28b4d03
kotec666 <alexkotov639@gmail.com>
2024-04-22 15:17:02 +0400

    logo

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = (props: IProps) => {
 	return (
 		<div className='bg-grey-313 h-[70px] flex justify-between items-center px-[20px]'>
 			<Link href='/'>
-				<Image src='/svg/logo.svg' alt='logo' width={50} height={50} />
+				<Image src='/svg/logo.svg' alt='logo' width={55} height={55} />
 			</Link>
 			<div className='flex items-center gap-x-[15px]'>
 				<span className='text-sm'>{props.user?.name}</span>
